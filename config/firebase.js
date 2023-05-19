69406e8a7fc74a185a4c4f41f0b93d26cda3fd0f
Lucas Silva <lucassilva07918@gmail.com>
2023-05-19 02:02:58 -0300

    Primeiro commit

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDP8rwEaMKzLtRaoJgaRtI6T1bIs2DT2QA",
+  authDomain: "foco-project.firebaseapp.com",
+  projectId: "foco-project",
+  storageBucket: "foco-project.appspot.com",
+  messagingSenderId: "187241132786",
+  appId: "1:187241132786:web:f862c8ebb69af66c00d66b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
